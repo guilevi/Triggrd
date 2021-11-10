c4684a2ffb1b1353d2f91aff0024cc4f88aa988e
guilevi <guilevi2000@gmail.com>
2021-11-10 12:53:59 +0000

    Add authentication event (horribly done) and a couple of more things

--- Triggrd.spoon/axobserver.lua
+++ Triggrd.spoon/axobserver.lua
@@ -5,13 +5,13 @@ end
 local element = hs.axuielement.applicationElement(app)
 local observer = hs.axuielement.observer.new(app:pid())
 pcall(function()
-    -- observer:addWatcher(element, hs.axuielement.observer.notifications.windowCreated)
     observer:addWatcher(element, hs.axuielement.observer.notifications.menuOpened)
     observer:addWatcher(element, hs.axuielement.observer.notifications.menuClosed)
     observer:addWatcher(element, hs.axuielement.observer.notifications.menuItemSelected)
     observer:addWatcher(element, hs.axuielement.observer.notifications.focusedWindowChanged)
     observer:addWatcher(element, hs.axuielement.observer.notifications.windowMiniaturized)
     observer:addWatcher(element, hs.axuielement.observer.notifications.windowDeminiaturized)
+    observer:addWatcher(element, hs.axuielement.observer.notifications.sheetCreated)
 end)
 observer:callback(function(observer, element, type, notificationInfo)
     local tags = {}
@@ -30,11 +30,28 @@ observer:callback(function(observer, element, type, notificationInfo)
         data = {
             textArgs = {element:attributeValue(hs.axuielement.attributes.title)}
         }
+    elseif type == hs.axuielement.observer.notifications.windowMiniaturized then
+        tags = {'window', 'minimized', 'miniaturized', element:attributeValue(hs.axuielement.attributes.title)}
+        data = {
+            textArgs = {element:attributeValue(hs.axuielement.attributes.title)}
+        }
+    elseif type == hs.axuielement.observer.notifications.windowDeminiaturized then
+        tags = {'window', 'deminimized', 'deminiaturized', element:attributeValue(hs.axuielement.attributes.title)}
+        data = {
+            textArgs = {element:attributeValue(hs.axuielement.attributes.title)}
+        }
+    elseif type == hs.axuielement.observer.notifications.sheetCreated then
+        tags = {'sheetcreated'}
+        -- This is absolutely horrible but I literally cannot think of another way of doing this
+if element:attributeValue('AXChildren')[3] and element:attributeValue('AXChildren')[3]:attributeValue('AXValue'):find("is trying to") then
+table.insert(tags, 'authentication')
+end
     end
     Triggrd:handleEvent({
         tags = tags,
         data = data
     })
+
 end)
 observer:start()
 
